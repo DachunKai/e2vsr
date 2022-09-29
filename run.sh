b0675850717982dc6544690e61df7d52c829d9d6
DachunKai <i.dachunkai@gmail.com>
2022-09-29 13:12:52 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 pip install -r requirements.txt && \
 python setup.py develop && \
 sed -i -e 's/\r$//' ./scripts/dist_train.sh && \
-pip install einops && \
+pip install einops matplotlib && \
 ./scripts/dist_train.sh 4 options/train/BasicVSR/train_e2vsr_CED.yml
