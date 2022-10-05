9dced6ecb2ccea094ca659a0f899393ceb007b5e
DachunKai <i.dachunkai@gmail.com>
2022-10-05 15:33:42 +0000

    add meta_info_CED_3_h5.txt

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-pip install -r requirements.txt && \
-python setup.py develop && \
-sed -i -e 's/\r$//' ./scripts/dist_train.sh && \
-pip install einops matplotlib && \
-./scripts/dist_train.sh 4 options/train/BasicVSR/train_e2vsr_CED.yml
+# !/bin/bash
+python -W ignore scripts/data_preparation/create_hdf5.py &&
+CUDA_VISIBLE_DEVICES=0,2 ./scripts/dist_train.sh 2 options/train/BasicVSR/train_e2vsr_CED.yml
