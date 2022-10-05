b3b9cd0381e2a8e7a0df32dc2511e5ef5bfd6d23
DachunKai <i.dachunkai@gmail.com>
2022-10-05 15:27:36 +0000

    add meta_info_CED_3_h5.txt

--- scripts/data_preparation/create_hdf5.py
+++ scripts/data_preparation/create_hdf5.py
@@ -60,7 +60,7 @@ def prepare_h5_keys_ced_lr(folder_path):
         if 'images' in subdir:
             root_path_list.append(root)
             h5_hr_path_list.append(osp.join('datasets/CED_3_h5/Voxel_3/HR', osp.basename(root) + '.h5'))
-            h5_lr_path_list.append(osp.join('datasets/CED_3_h5/Voxel_3/LRx2', osp.basename(root) + '.h5'))
+            h5_lr_path_list.append(osp.join('datasets/CED_3_h5/Voxel_3/LRx4', osp.basename(root) + '.h5'))
 
     return root_path_list, h5_hr_path_list, h5_lr_path_list
 
@@ -71,7 +71,7 @@ def create_hdf5_for_ced_lr():
         Remember to modify opt configurations according to your settings.
     """
     # HR
-    folder_path = 'datasets/CED_3/LRx2'
+    folder_path = 'datasets/CED_3/LRx4'
     root_path_list, h5_hr_path_list, h5_lr_path_list = prepare_h5_keys_ced_lr(folder_path)
     make_lr_hdf5_from_folders(root_path_list, h5_hr_path_list, h5_lr_path_list)
 
