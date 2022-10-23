d75f5852956321d4ccd0f13187e0af4fad55a96c
DachunKai <i.dachunkai@gmail.com>
2022-10-23 16:20:20 +0000

    1080ti backup

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Wed Sep 21 04:38:26 2022
+# TIME: Sun Oct 23 16:17:45 2022
 __version__ = '1.4.2'
-__gitsha__ = '1aed286'
+__gitsha__ = '8924e99'
 version_info = (1, 4, 2)
